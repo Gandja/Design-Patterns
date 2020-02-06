6f330eb7d33a75e465bd8a7e14558cc674309387
Denis Gandzha <denis.gandzha@artezio.com>
2020-02-06 10:34:51 +0300

    creational patterns

---
+++ creational patterns/singleton/ProgramLogger.java
@@ -0,0 +1,26 @@
+package singleton;
+
+public class ProgramLogger {
+    private static ProgramLogger programLogger;
+    private static String logFile = "This is log file..";
+
+    public static synchronized ProgramLogger getProgramLogger() {
+        if(programLogger == null) {
+            programLogger = new ProgramLogger();
+        }
+
+        return programLogger;
+    }
+
+    private ProgramLogger() {
+
+    }
+
+    public void addLogInfo(String logInfo) {
+        logFile += logInfo;
+    }
+
+    public void showLogFile() {
+        System.out.println(logFile);
+    }
+}
